db4f0e6936a54551fc6cb5daf6df5bdada40300b
lagwy <lagwyf@gmail.com>
2016-08-28 05:00:30 -0500

    User navbar and mysql data updated

--- pages/navbar.php
+++ pages/navbar.php
@@ -1,46 +1,62 @@
+<?php
+requiere('stack.php');
+$stack = new Stack();
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
+    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>TourLine</title>
-	<link rel="icon" href="">
-	<link rel="stylesheet" type="text/css" href="assets/bootstrap/css/bootstrap.css">
-	<link rel="stylesheet" href="assets/css/normalize.css">
+    <title>TourLine</title>
+    <link rel="icon" href="">
+    <link rel="stylesheet" type="text/css" href="assets/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/normalize.css">
     <link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
-	 <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 </head>
-	
-	<div class="container">
-	<div class="col-md-12">
-	    <nav class="navbar navbar-default navbar-fixed-top" role="navigation" style="margin-bottom: 0">
-	        <div class="container">
-	          <!-- Brand and toggle get grouped for better mobile display -->
-	          	<div class="navbar-header">
-	            	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
-	              		<span class="sr-only">Toggle navigation</span>
-	              		<span class="icon-bar"></span>
-	              		<span class="icon-bar"></span>
-	              		<span class="icon-bar"></span>
-	            	</button>
-	            	<span class="navbar-brand" href=""><img src="" alt=""></i>&nbsp;<b style="color:#F2720A">TOUR</b><b style="color:#41CC25">LINE</b></span>
-	          	</div>
-	          	<!-- Collect the nav links, forms, and other content for toggling -->
-	          	<div class="collapse navbar-collapse" id="navbar-collapse">
-	            	<ul class="nav navbar-nav navbar-right nav nav-pills" id="nav">
-	              		<li><a href="#home">Inicio</a></li>
-	                	<li><a href="#multimedia">Registrarse</a></li>
-	                	<li class="active"><a>Inicio de Sesión</a><li>
-	            	</ul>
-	          	</div><!-- /.navbar-collapse -->
-	        </div><!-- /.container-fluid -->
-	    </nav>
-	  </div>
-	</div>
+
+<div class="container">
+    <div class="col-md-12">
+        <nav class="navbar navbar-default navbar-fixed-top" role="navigation" style="margin-bottom: 0">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <span class="navbar-brand" href=""><img src="" alt=""></i>&nbsp;<b style="color:#F2720A">TOUR</b><b
+                            style="color:#41CC25">LINE</b></span>
+                </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="navbar-collapse">
+                    <ul class="nav navbar-nav navbar-right nav nav-pills" id="nav">
+                        <li><a href="#home">Inicio</a></li>
+                        <?php
+                        if ($stack->isLogged()) {
+                            $usuario = $stack->getUsuarioActual();
+                            $nombre = $usuario['nombre'];
+                            echo "<li><a href='#multimedia'>$nombre</a></li>";
+                            echo "<li><a href='stack.php?logout=1'>Logout</a></li>";
+                        } else {
+                            echo "<li class=\"active\"><a href='user_login.php'>Registro</a><li>
+									<li class=\"active\"><a href='user_login.php'>Inicio de Sesión</a><li>";
+                        }
+
+                        ?>
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
+        </nav>
+    </div>
+</div>
 
     
